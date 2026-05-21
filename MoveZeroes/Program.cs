c877c72b47bc056a32da70f290dd236f78193c24
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-05-21 12:48:12 +0100

    LC Two Sum (Easy) - HashMap one pass LC Move Zeroes (Easy) - Two pointer slow/fast

---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,17 @@
+﻿class Solution{
+    static int[] moveZeroes(int[] arr){
+        for(int i =0, j= 0; j< arr.Length; j++){
+            if(arr[j] != 0){
+                (arr[i], arr[j]) = (arr[j], arr[i]);
+                i++;
+            }
+        }
+       
+        return arr;
+    }
+    public static void Main(String[] args){
+        int[] arr = new int[] {1,0, 0 ,3,12};
+        Console.WriteLine($"{string.Join(" ", moveZeroes(arr))}");
+        
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,21 @@
+﻿class Solution{
+    static int[] indicesOfSum(int[] arr, int target){
+        Dictionary<int, int> numberWithIndices = new();
+        for(int i = 0; i< arr.Length; i++){
+            int rem = target - arr[i];
+            if(numberWithIndices.GetValueOrDefault(rem, -1) == -1){
+                numberWithIndices[arr[i]] = i; 
+            }
+            else{
+                return new int[]{numberWithIndices[rem], i};
+            }
+        }
+        return new int[2];
+    }
+    public static void Main(String[] args){
+        int[] arr = new int[] {2, 7, 11, 15};
+        int target = 9;
+        Console.WriteLine($"{string.Join(" ", indicesOfSum(arr, target))}");
+        
+    }
+}
