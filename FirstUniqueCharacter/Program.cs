4072bdb215a2e4d287452bdc4c2456938eb3ac71
pmishra24 <46805236+pmishra24@users.noreply.github.com>
2026-05-21 13:04:12 +0100

    LC First Unique Character (Easy) - int[26] two-pass

---
+++ FirstUniqueCharacter/Program.cs
@@ -0,0 +1,19 @@
+﻿class Solution{
+    static int FirstUniqueCharacterIndex(string word){
+        int[] charCount = new int[26];
+
+        for(int i =0; i< word.Length; i++){
+            charCount[word[i] - 'a']++;
+        }
+        for(int i =0; i< word.Length; i++){
+            if(charCount[word[i] - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+    public static void Main(String[] args){
+        string word = "prasharp";
+        Console.WriteLine($"{FirstUniqueCharacterIndex(word)}");
+        
+    }
+}
